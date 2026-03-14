0a9747eedb650ea74ef8c271e1cd36b87487b9bb
ISHANT PATIL <165829237+Ishantpatil1@users.noreply.github.com>
2026-03-14 14:44:29 +0530

    Update javascript-test-0001.js

--- javascript-test-0001.js
+++ javascript-test-0001.js
@@ -1,15 +1,29 @@
-const n = 5;
-    for (let i = 1; i <= (2 * n - 1); i++) {
-  let row = "";
- let distances = i <= n ? n - i : i - n;
-     let widthes = 2 * n - 1;
- for (let j = 0; j < widthes; j++) {
-      let leftstars = distances;
-    let rightstars = widths - distances - 1;
-      if (j === leftstars || j === rightstars) {
-     row += "*";
-       } else {
-         row += " ";
-          }    }
-   console.log(row);
+const n = 5; // half height of diamond
+
+for (let i = 1; i <= (2 * n - 1); i++) {
+
+    let row = "";
+    
+    // distance from middle line
+    let dist = i <= n ? n - i : i - n;
+
+    // total width of pattern
+    let width = 2 * n - 1;
+
+    for (let j = 0; j < width; j++) {
+
+        // left star position
+        let leftStar = dist;
+
+        // right star position
+        let rightStar = width - dist - 1;
+
+        if (j === leftStar || j === rightStar) {
+            row += "*";
+        } else {
+            row += " ";
+        }
+    }
+
+    console.log(row);
 }
