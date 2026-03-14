bf25c3aa93a6c5aacfd63f9306d9e672debb171a
ISHANT PATIL <165829237+Ishantpatil1@users.noreply.github.com>
2026-03-14 14:12:25 +0530

    Refactor star pattern printing logic in JavaScript

--- javascript-test-0001.js
+++ javascript-test-0001.js
@@ -1,15 +1,15 @@
 const n = 5;
     for (let i = 1; i <= (2 * n - 1); i++) {
-  let rows = "";
+  let row = "";
  let distances = i <= n ? n - i : i - n;
      let widthes = 2 * n - 1;
  for (let j = 0; j < widthes; j++) {
       let leftstars = distances;
     let rightstars = widths - distances - 1;
       if (j === leftstars || j === rightstars) {
-     rows += "*";
+     row += "*";
        } else {
-         rows += " ";
+         row += " ";
           }    }
-   console.log(rows);
+   console.log(row);
 }
